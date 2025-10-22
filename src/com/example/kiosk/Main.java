89d56c2c02277b659bb78769ed8ed8a94095b4e5
jiwon jung <wldnjs2277@gmail.com>
2025-10-22 13:12:59 +0900

    refactor: 필수 기능 Level2 객체 지향 설계 적용 구현

--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -1,21 +1,29 @@
 package com.example.kiosk;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<MenuItem> menuItems = new ArrayList<>();
+
+        menuItems.add(new MenuItem(1, "ShackBurger", 6900, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem(2, "ShackBurger", 8900, "베이컨, 체리 페어에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem(3, "ShackBurger", 6900, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem(4, "ShackBurger", 5400, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
+        System.out.println("==================== [ SHAKESHACK MENU ] ====================");
+        for (MenuItem menuItem : menuItems) {
+            System.out.println(menuItem.id + ". " + menuItem.name + " | " + menuItem.price + " | " + menuItem.description);
+        }
+        System.out.println("0. 종료         | 종료");
+        System.out.println("=============================================================");
 
         while (true) {
-            System.out.println("==================== [ SHAKESHACK MENU ] ====================");
-            System.out.println("1. ShackBurger  | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-            System.out.println("2. ShackBurger  | W 8.9 | 베이컨, 체리 페어에 쉑소스가 토핑된 치즈버거");
-            System.out.println("3. ShackBurger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-            System.out.println("4. ShackBurger  | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
-            System.out.println("0. 종료          | 종료");
-            System.out.println("=============================================================");
 
             System.out.println();
             System.out.print("메뉴를 보고 번호를 선택하세요: ");
@@ -34,10 +42,8 @@ public class Main {
                     }
                     default -> System.out.println("존재하지 않는 메뉴입니다.");
                 }
-                System.out.println();
             } catch (InputMismatchException e) {
                 System.out.println("올바른 메뉴를 선택해주세요.");
-                System.out.println();
                 sc.nextLine(); // 버퍼 비우기
             }
         }
---
+++ src/com/example/kiosk/MenuItem.java
@@ -0,0 +1,15 @@
+package com.example.kiosk;
+
+public class MenuItem {
+    int id;
+    String name;
+    int price;
+    String description;
+
+    public MenuItem(int id, String name, int price, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+}
