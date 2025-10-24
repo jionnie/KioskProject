72b71144518b1d9a21efbc27ae47d19b02282528
jiwon jung <wldnjs2277@gmail.com>
2025-10-24 11:13:16 +0900

    refactor: MenuItem 클래스 javadoc 주석 추가 & Main 클래스 try 블록 최소화

--- src/com/example/kiosk/MenuItem.java
+++ src/com/example/kiosk/MenuItem.java
@@ -1,11 +1,23 @@
 package com.example.kiosk;
 
+/**
+ * 개별 음식 항목을 관리하는 클래스
+ * @author jiwon jung
+ */
 public class MenuItem {
     int id;
     String name;
     int price;
     String description;
 
+    /**
+     * 새로운 메뉴를 생성한다.
+     *
+     * @param id 메뉴 번호(고유 식별자)
+     * @param name 메뉴 이름
+     * @param price 메뉴 가격(단위: 원)
+     * @param description 메뉴 설명
+     */
     public MenuItem(int id, String name, int price, String description) {
         this.id = id;
         this.name = name;
